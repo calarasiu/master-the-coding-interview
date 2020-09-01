ac3a5227426ff53894627ad91dd8b6a3f6c6d41e
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-09-01 10:22:01 +0100

    O(n) - linear time

--- bigOandScalability.js
+++ bigOandScalability.js
@@ -11,6 +11,11 @@ function findNemo(array){
   }
   let t1 = performance.now();
   console.log('Found Nemo in '+ (t1-t0) +'milliseconds');
+
+  // the problem with measuring the performance is that the input is not the only variable that influences it - the CPU of the computer matters as well, what else is running in the mean time etc
+
+  // the big O notation measures only the growth of the operations related to the growth of the input
+  // this function has a linear time growth - O(n) - n is the number of elements - for each element there will be another operation that will run 
 }
 
 findNemo(large);
