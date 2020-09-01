4fab289f22c5fb7db9a5a661895da0c18a363e92
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-09-01 10:55:00 +0100

    calculate the Big O of a function

---
+++ bigOexercise1.js
@@ -0,0 +1,14 @@
+function funChallenge(input){
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  input.forEach(element => { // O(n)
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
+  });
+  // what's inside the for loop it will run for each element of the input - that means it has a time of execution of O(n)
+  return a; // O(1)
+}
+
+Big O(3 + 4n) = O(n) 
