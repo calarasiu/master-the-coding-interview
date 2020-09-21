0a69b52415fc94690903fcf57501ce7e2a50d65d
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-09-21 19:11:38 +0100

    interview cake

--- linked_lists/linkedLists.js
+++ linked_lists/linkedLists.js
@@ -15,6 +15,7 @@ class Node{
   constructor(value){
     this.value= value;
     this.next = null;
+    this.previous=null;
   }
 };
 
@@ -22,13 +23,15 @@ class LinkedLists{
   constructor(value){
     this.head = {
       value: value,
-      next:null
+      next:null,
+      previous:null
     }
     this.tail = this.head;
     this.length = 1;
   }
   append(value){
     const appendNode = new Node(value);
+    appendNode.previous = this.tail;
     this.tail.next = appendNode;
     this.tail = appendNode;
     this.length+=1;
@@ -37,6 +40,7 @@ class LinkedLists{
 
   prepend(value){
     const prependNode = new Node(value);
+    this.head.previous = prependNode;
     prependNode.next = this.head;
     this.head = prependNode;
     this.length+=1;
@@ -54,8 +58,8 @@ class LinkedLists{
   }
 
   // *  -  *   => *       *  => *       *   =>  * - * - * 
-  //                \             \   /
-  //                  *             *
+  //                \            \   /
+  //                 *             *
 
   insert(index, value){
     if(index >= this.printList().length-1){
@@ -67,8 +71,10 @@ class LinkedLists{
       const insertNode = new Node(value);
       const previousNode = this.traverseToIndex(index-1);
       let holdingPointer = previousNode.next;
+      holdingPointer.previous = insertNode;
       previousNode.next = insertNode;
       insertNode.next = holdingPointer;
+      insertNode.previous = previousNode;
       this.length++;
     }
     return this.printList();
@@ -87,12 +93,35 @@ class LinkedLists{
       const previousNode = this.traverseToIndex(index-1);
       const holdingPointer = (previousNode.next).next;
       previousNode.next.remove;
+      holdingPointer.previous = previousNode;
       previousNode.next = holdingPointer;
     }
     this.length--;
     return this.printList();
   }
-
+  reverse(){
+    if(!this.head.next){
+      return this.head;
+    }
+    let first= this.head;
+    this.tail = this.head;
+    let second = first.next;
+    let iteration = 0;
+    while(second){
+      console.log("iteraion", iteration++);
+      const temp = second.next;
+      console.log("temp", temp);
+      second.next = first;
+      console.log("second.next", second.next);
+      first=second;
+      console.log("first", first);
+      second = temp;
+      console.log("second", second);
+    }
+    this.head.next = null;
+    console.log(this.head);
+    this.head = first;
+  }
   traverseToIndex(index){
     let counter = 0;
     let currentNode = this.head;
@@ -107,7 +136,10 @@ class LinkedLists{
 const myLinkedList = new LinkedLists(10);
 myLinkedList.append(5);
 myLinkedList.prepend(1);
+
 myLinkedList.insert(1, 77);
-console.log("before removing",myLinkedList.printList());
-myLinkedList.remove(3);
+// console.log("before removing",myLinkedList.printList());
+// myLinkedList.remove(2);
+console.log(myLinkedList.printList());
+myLinkedList.reverse();
 console.log(myLinkedList.printList());
