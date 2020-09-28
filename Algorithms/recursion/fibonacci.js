028a545be136e4cc73fe40a4c6c3c749473b193f
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-09-28 13:41:02 +0100

    fibonacci series

---
+++ Algorithms/recursion/fibonacci.js
@@ -0,0 +1,30 @@
+function fibonacciIterative(n){
+  const results = [];
+  while(results.length<=n){
+    if(results.length === 0){
+      console.log("first if")
+      results.push(0);
+    }else if(results.length ===1){
+      console.log("2 if")
+      results.push(1);
+    }else{
+      console.log("3 if")
+      last = results[results.length-1];
+      lastButOne = results[results.length-2];
+      results.push(last+lastButOne);
+    }
+  }
+  return results[results.length-1];
+}
+
+// console.log(fibonacciIterative(8));
+
+function fibRecursive(n){
+  debugger
+      if(n<2){
+      return 1;
+      }
+      
+  return fibRecursive(n-1) + fibRecursive(n-2);
+  }
+  console.log(fibRecursive(3));
